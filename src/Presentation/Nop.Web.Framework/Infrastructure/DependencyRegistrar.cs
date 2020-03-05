31c0a8cbbc904cb46e895f3f0ddb185d3ae1165b
skoshelev <serega.dn@gmail.com>
2020-03-05 17:42:19 +0300

    3475 Fixed problem with registration the memory cache in Autofac

--- src/Presentation/Nop.Web.Framework/Infrastructure/DependencyRegistrar.cs
+++ src/Presentation/Nop.Web.Framework/Infrastructure/DependencyRegistrar.cs
@@ -6,7 +6,6 @@ using Autofac;
 using Autofac.Builder;
 using Autofac.Core;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Microsoft.Extensions.Caching.Memory;
 using Nop.Core;
 using Nop.Core.Caching;
 using Nop.Core.Configuration;
@@ -114,7 +113,6 @@ namespace Nop.Web.Framework.Infrastructure
             {
                 builder.RegisterType<MemoryCacheManager>()
                     .As<ILocker>()
-                    .As<IMemoryCache>()
                     .As<IStaticCacheManager>()
                     .SingleInstance();
             }
